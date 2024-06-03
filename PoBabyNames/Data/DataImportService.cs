8b470ca2179e7cbb05b2c4ad68783c44f29a7f94
punkouter24 <punkouter24@gmail.com>
2024-06-03 13:18:10 -0400

    ...

--- PoBabyNames/Data/DataImportService.cs
+++ PoBabyNames/Data/DataImportService.cs
@@ -21,6 +21,41 @@ public class DataImportService
         await _dbContext.SaveChangesAsync();
     }
 
+    //public async Task ImportCsvDataAsync(Stream fileStream)
+    //{
+    //    try
+    //    {
+    //        await ClearNamesTableAsync();  // Clear the table before importing new data
+
+    //        using var reader = new StreamReader(fileStream);
+    //        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true });
+    //        var records = csv.GetRecords<Name>().ToList(); // Convert to List to catch any parsing errors
+    //        if (records.Any())
+    //        {
+    //            _dbContext.Names.AddRange(records);
+    //            await _dbContext.SaveChangesAsync();
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("No records found to import.");
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Console.WriteLine($"An error occurred: {ex.Message}");
+    //    }
+    //}
+
+    //public async Task ImportCsv(Stream fileStream)
+    //{
+    //    await ImportCsvDataAsync(fileStream);
+    //}
+
+
+
+
+
+
     public async Task ImportCsvDataAsync()
     {
         try
@@ -28,12 +63,12 @@ public class DataImportService
             await ClearNamesTableAsync();  // Clear the table before importing new data
 
             // string filePath = "NationalNames.csv";
-             string filePath = "names100.csv";
+            string filePath = "names100.csv";
 
             using var reader = new StreamReader(filePath);
             using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true });
             var records = csv.GetRecords<Name>().ToList(); // Convert to List to catch any parsing errors
-           // var records = csv.GetRecords<Name>().Take(10).ToList(); // Fetch only the first 10 records
+                                                           // var records = csv.GetRecords<Name>().Take(10).ToList(); // Fetch only the first 10 records
             if (records.Any())
             {
                 _dbContext.Names.AddRange(records);
